7972d858837f00240bd9fddf92157f5683214835
williamtenhunen <williamtenhunen@gmail.com>
2025-07-12 13:01:34 +0300

    Add Two Sum solution (Easy)

---
+++ Easy/TwoSum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+solution = Solution()
+
+print(solution.twoSum([4, 5, 11, 15], 9))
