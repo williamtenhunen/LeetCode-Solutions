591aebc82a8c034ed946b20ccd54f232d3b04bf7
williamtenhunen <williamtenhunen@gmail.com>
2025-07-19 13:42:06 +0300

    Add Search Insert Position solution (Easy)

---
+++ Easy/SearchInsertPosition.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid
+        return left
+
+solution = Solution()
+print(solution.searchInsert([1,3,5,6], 5))
+print(solution.searchInsert([1,3,5,6], 2))
+print(solution.searchInsert([1,3,5,6], 7))
