b16db316a88881941930364ae6e62147306c1a2b
williamtenhunen <williamtenhunen@gmail.com>
2025-08-01 16:33:28 +0300

    Add Balanced Binary Tree solution (Easy)

---
+++ Easy/BalancedBinaryTree.py
@@ -0,0 +1,20 @@
+from typing import Optional, List
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def check(node):
+            if not node:
+                return 0
+            left_height = check(node.left)
+            right_height = check(node.right)
+            if left_height == -1 or right_height == -1 or abs(left_height - right_height) > 1:
+                return -1
+            return max(left_height, right_height) + 1
+        
+        return check(root) != -1
